ff168340e77fc4bd02531c41a103e880264d329e
Brock Guzman <bmg323@gmail.com>
2020-04-30 14:51:08 -0400

    Updated for PythonAnywhere

--- SubtoneSelector/SubtoneSelector/spotify_support.py
+++ SubtoneSelector/SubtoneSelector/spotify_support.py
@@ -7,13 +7,14 @@ import spotipy.util as util
 import pandas as pd
 import sqlite3
 from django_pandas.io import read_frame
+import urllib.request
 
 
-SPOTIPY_REDIRECT_URI = 'http://localhost:8000/logged/'
+SPOTIPY_REDIRECT_URI = 'http://bromasan.pythonanywhere.com/logged/'
 
 SPOTIPY_CLIENT_ID = 'c6b73836172b40b2ac90879f9b54271b'
 SPOTIPY_CLIENT_SECRET = 'bced1ccc150b4ee5b65f295b98e33b95'
-CACHE_PATH = '.cache_spot'
+CACHE_PATH = '.cache'
 token=''
 username=''
 
@@ -33,12 +34,20 @@ def login_spotify(user):
 
     global token, username
     username = user
+    if username not in sp_oauth.cache_path:
+        sp_oauth.cache_path = '.cache_' + username
 
     token_info = sp_oauth.get_cached_token()
 
     if not token_info:
-        code = sp_oauth.get_auth_response()
-        token = sp_oauth.get_access_token(code, as_dict=False)
+        auth_url = sp_oauth.get_authorize_url()
+        return auth_url
+        # with urllib.request.urlopen(auth_url) as response:
+        #     html = response.read()
+        #     print(html)
+        # sp_oauth._open_auth_url()
+        # code = sp_oauth.get_auth_response()
+        # token = sp_oauth.get_access_token(code, as_dict=False)
     else:
         token = token_info['access_token']
 
