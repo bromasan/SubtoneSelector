d76e19db23a729bc0d0c7f47d7d6090c00b16104
bromasan <64594954+bromasan@users.noreply.github.com>
2020-07-19 13:26:12 -0400

    Update forms.py

--- SubtoneSelector/SubtoneSelector/Recommendation/forms.py
+++ SubtoneSelector/SubtoneSelector/Recommendation/forms.py
@@ -3,7 +3,7 @@ from django.core import validators
 from Recommendation.models import BigArtist
 
 class ArtistForm(forms.Form):
-    name = forms.CharField(max_length = 64)
+    name = forms.CharField(max_length = 64, widget=forms.TextInput(attrs={'class': 'search-form', 'placeholder': 'Artist Name'}), label= '')
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length = 64)
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'search-form', 'placeholder': 'Enter Spotify Username'}), label= '',max_length = 64)
